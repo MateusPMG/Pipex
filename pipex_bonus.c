351a1af3eefc8e69273f98380ba7e095601ee9e0
MateusPMG <mpmgboss@gmail.com>
2023-05-03 12:00:40 +0100

    almost solved

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,26 +6,16 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 11:56:07 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/05/02 19:29:25 by mpatrao          ###   ########.fr       */
+/*   Updated: 2023/05/03 11:58:48 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
-void	open_file(int ac, char **av, t_files *file)
-{
-	file->fdin = open(av[1], O_RDONLY);
-	file->fdout = open(av[ac - 1], O_RDWR | O_CREAT | O_TRUNC, 0644);
-	dup2(file->fdin, STDIN_FILENO);
-}
-
-void	here_doc(int ac, char **av, t_files *file)
+void	write_to_pipe(int *fd, char **av)
 {
 	char	*buf;
 
-	file->fdin = open(".here_doc", O_RDWR | O_CREAT | O_TRUNC, 0644);
-	file->fdout = open(av[ac - 1], O_RDWR | O_CREAT | O_APPEND, 0644);
-	file->here = 1;
 	while (1)
 	{
 		write(1, "here_doc>", 9);
@@ -33,14 +23,14 @@ void	here_doc(int ac, char **av, t_files *file)
 		if (!buf || (!ft_strncmp(buf, av[2], ft_strlen(av[2]))
 				&& buf[ft_strlen(av[2])] == '\n'))
 			break ;
-		write(file->fdin, buf, ft_strlen(buf));
+		write(*fd, buf, ft_strlen(buf));
 		free(buf);
 	}
-	dup2(file->fdin, STDIN_FILENO);
 	free(buf);
+	close(*fd);
 }
 
-void	redirect(char *cmd, char **env, int in_fd, int out_fd)
+int	here_doc(char **av)
 {
 	pid_t	pid;
 	int		pipefd[2];
@@ -56,14 +46,64 @@ void	redirect(char *cmd, char **env, int in_fd, int out_fd)
 	if (pid == 0)
 	{
 		close(pipefd[0]);
-		dup2(in_fd, STDIN_FILENO);
+		write_to_pipe(&pipefd[1], av);
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+		close(pipefd[1]);
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[0]);
+	}
+	return (STDIN_FILENO);
+}
+
+int	start_files(int ac, char **av, t_files *file)
+{
+	int	i;
+
+	if (!ft_strncmp(av[1], "here_doc", 8))
+	{
+		i = 3;
+		file->fdin = here_doc(av);
+		file->fdout = open(av[ac - 1], O_RDWR | O_CREAT | O_APPEND, 0644);
+	}
+	else
+	{
+		i = 2;
+		file->fdin = open(av[1], O_RDONLY);
+		file->fdout = open(av[ac - 1], O_RDWR | O_CREAT | O_TRUNC, 0644);
+	}
+	return (i);
+}
+
+
+
+
+
+void	redirect(char *cmd, char **env)
+{
+	pid_t	pid;
+	int		pipefd[2];
+
+	if (pipe(pipefd) == -1)
+	{
+		perror("Error");
+		exit(1);
+	}
+	pid = fork();
+	if (pid == -1)
+		exit(1);
+	if (pid == 0)
+	{
+		close(pipefd[0]);
 		dup2(pipefd[1], STDOUT_FILENO);
 		use_command(cmd, env);
 	}
 	else
 	{
 		close(pipefd[1]);
-		dup2(pipefd[0], out_fd);
+		dup2(pipefd[0], STDIN_FILENO);
 		waitpid(pid, NULL, 0);
 	}
 }
@@ -80,19 +120,15 @@ int	main(int ac, char **av, char **env)
 		return (write(1, "Error, must be at least 4 arguments.\n", 37));
 	else
 	{
-		if (!ft_strncmp("here_doc", av[1], ft_strlen(av[1])))
-			here_doc(ac, av, &file);
-		else
-			open_file(ac, av, &file);
-		i = 2 + file.here;
+		i = start_files(ac, av, &file);
+		dup2(file.fdin, STDIN_FILENO);
+		dup2(file.fdout, STDOUT_FILENO);
 		while (i < ac - 2)
 		{
-			redirect(av[i], env, STDIN_FILENO, STDOUT_FILENO);
+			redirect(av[i], env);
 			i++;
 		}
-		dup2(file.fdout, STDOUT_FILENO);
-		use_command(av[ac - 2], env);
+		use_command(av[i], env);
 	}
-	unlink(".here_doc");
 	return (1);
 }
